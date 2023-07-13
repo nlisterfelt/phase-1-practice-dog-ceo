141d3903c0c75777cc8511c87517d48dcb162f44
Nicole Listerfelt <nlisterfelt@gmail.com>
2023-07-12 21:49:40 -0500

    challenge 1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+function init(){
+    fetch(imgUrl)
+    .then(resp=>resp.json())
+    .then(data => addImg(data.message))
+}
+document.addEventListener('DOMContentLoaded', init)
+
+function addImg(data){
+    let imgObj = document.getElementById('dog-image-container')
+    for(let i=0; i<parseInt(data.length); i++){
+        const image = document.createElement('img')
+        image.src = data[i]
+        imgObj.appendChild(image)
+        console.log(imgObj)
+    }
+}   
